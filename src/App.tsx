9f631d26f26a5bd89ac12c8f062b779e65d70bdd
Arthur Duarte <tuiduarte@gmail.com>
2024-10-22 19:49:34 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <div>
+        <h1>Imagem</h1>
+      </div>
+      <div>
+        <h1>Sign Up</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
