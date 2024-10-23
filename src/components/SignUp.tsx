6b0e74c666b840190518360e1c340e591ff0ffee
Arthur Duarte <tuiduarte@gmail.com>
2024-10-22 20:20:07 -0400

    add shadcn/ui

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,5 +1,13 @@
 export default function SignUp() {
-  return (
-    <div>SignUp</div>
-  )
+    return (
+        <>
+        <div>
+            <h1>Steel Stacker's Newsletter</h1>
+            <h2>Never be the last one to know about new events, details, and fun stuff.</h2>
+        </div>
+        <div>
+            
+        </div>
+        </>
+    )
 }
