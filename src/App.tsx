a0d0ce883d15312c52405ff577973515ecaa64ff
Arthur Duarte <tuiduarte@gmail.com>
2024-10-24 22:47:23 -0400

    add responsive style

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import SignUp from './components/SignUp'
 function App() {
   return (
     <div className='flex flex-col lg:flex-row'>
-      <div className='w-1/3 text-center'>
+      <div className='lg:w-1/3 text-center'>
         <Image />
       </div>
-      <div className='w-2/3 flex flex-col justify-center items-center'>
+      <div className='lg:w-2/3 lg:my-0 my-5 mx-5 text-center flex flex-col justify-center items-center'>
         <SignUp />
       </div>
     </div>
