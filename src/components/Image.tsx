952d1abce12f3696486272a8032558bfa2df8c81
Arthur Duarte <tuiduarte@gmail.com>
2024-10-29 15:23:47 -0400

    add image hosted on aws s3

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,7 +1,7 @@
 export default function Image() {
     return (
         <>
-            <img src="src/assets/Logo.webp" className='w-full lg:h-svh object-cover' alt="Steel Stacker's image" />
+            <img src="https://steel-stackers-newsletter.s3.us-east-2.amazonaws.com/Logo.webp" className='w-full lg:h-svh object-cover' alt="Steel Stacker's image" />
         </>
     )
 }
